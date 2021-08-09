0d8ea7517682e969c5b3c98066c73986b87b9e75
Daniel Orchel <karma0rg4u@gmail.com>
2021-08-09 23:34:18 +0100

    Add useRef to get data from the form

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetup = () => {
   return (
-    <div>
-      New Meetu Page
-    </div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
   )
 }
 
