7c32bb4737f46d2458df2bb174cd5775d6379b71
Daniel Orchel <karma0rg4u@gmail.com>
2021-08-09 22:57:48 +0100

    Add components

--- src/pages/AllMeetup.jsx
+++ src/pages/AllMeetup.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://images.unsplash.com/photo-1604745055299-ea7488126d9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1075&q=80',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://images.unsplash.com/photo-1602797882193-51cb0e037534?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZHVibGlufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
 
 const AllMeetup = () => {
   return (
-    <div>
-      All Meetup Page
-    </div>
+    <section>
+      <MeetupList meetups={DUMMY_DATA} />
+    </section>
   )
 }
 
